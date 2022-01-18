6119a027f5d835bae12e4c862ad3371c2ec78f06
Sandeep9926 <96066823+Sandeep9926@users.noreply.github.com>
2022-01-18 11:54:57 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+export const cars = [
+  {
+    brand: "Hyundai Swift Dzire",
+    price: "Rs.5 lac",
+    color: "red",
+    mileage: "25kmpl",
+    url: "https://myridzassets.s3-ap-southeast-1.amazonaws.com/Manufacturers/Advaith_Hyundai/New_i20/Display.webp",
+  },
+  {
+    brand: "mahindra scorpio",
+    price: "Rs.12 lac",
+    color: "black",
+    mileage: "22kmpl",
+    url: "https://stat.overdrive.in/wp-content/odgallery/2020/04/56777_Mahindra-Scorpio_2020_002.jpg",
+  },
+  {
+    brand: "Toyoto Fortuner",
+    price: "Rs.40 lac",
+    color: "White",
+    mileage: "18kmpl",
+    url: "https://imgd.aeplcdn.com/1056x594/n/cw/ec/44709/fortuner-exterior-right-front-three-quarter-19.jpeg?q=85&wm=1",
+  },
+  {
+    brand: "Honda Verna",
+    price: "Rs.17 lac ",
+    color: "Blue",
+    mileage: "23kmpl",
+    url: "https://blog.gaadikey.com/wp-content/uploads/2018/09/2018-Hyundai-Verna-Special-Edition-Photo-1-Blue-Color.jpg",
+  },
+];
